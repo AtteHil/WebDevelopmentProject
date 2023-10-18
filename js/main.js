8b49758e20b3b6fcf146542e432a0020262b2841
Atte <attehiltunen@hotmail.com>
2023-10-18 10:38:59 +0300

    queries.js added and small adjustments to clarify the code

--- js/main.js
+++ js/main.js
@@ -1,727 +1,3 @@
-const  educationQuery ={
-  "query": [
-    {
-      "code": "Vuosi",
-      "selection": {
-        "filter": "item",
-        "values": [
-          "2022"
-        ]
-      }
-    },
-    {
-      "code": "Alue",
-      "selection": {
-        "filter": "agg:_Municipalities in alphabetical order 2023.agg",
-        "values": [
-          "SSS",
-          "KU020",
-          "KU005",
-          "KU009",
-          "KU010",
-          "KU016",
-          "KU018",
-          "KU019",
-          "KU035",
-          "KU043",
-          "KU046",
-          "KU047",
-          "KU049",
-          "KU050",
-          "KU051",
-          "KU052",
-          "KU060",
-          "KU061",
-          "KU062",
-          "KU065",
-          "KU069",
-          "KU071",
-          "KU072",
-          "KU074",
-          "KU075",
-          "KU076",
-          "KU077",
-          "KU078",
-          "KU079",
-          "KU081",
-          "KU082",
-          "KU086",
-          "KU111",
-          "KU090",
-          "KU091",
-          "KU097",
-          "KU098",
-          "KU102",
-          "KU103",
-          "KU105",
-          "KU106",
-          "KU108",
-          "KU109",
-          "KU139",
-          "KU140",
-          "KU142",
-          "KU143",
-          "KU145",
-          "KU146",
-          "KU153",
-          "KU148",
-          "KU149",
-          "KU151",
-          "KU152",
-          "KU598",
-          "KU165",
-          "KU167",
-          "KU169",
-          "KU170",
-          "KU171",
-          "KU172",
-          "KU176",
-          "KU177",
-          "KU178",
-          "KU179",
-          "KU181",
-          "KU182",
-          "KU186",
-          "KU202",
-          "KU204",
-          "KU205",
-          "KU208",
-          "KU211",
-          "KU213",
-          "KU214",
-          "KU216",
-          "KU217",
-          "KU218",
-          "KU224",
-          "KU226",
-          "KU230",
-          "KU231",
-          "KU232",
-          "KU233",
-          "KU235",
-          "KU236",
-          "KU239",
-          "KU240",
-          "KU320",
-          "KU241",
-          "KU244",
-          "KU245",
-          "KU249",
-          "KU250",
-          "KU322",
-          "KU256",
-          "KU257",
-          "KU260",
-          "KU261",
-          "KU263",
-          "KU265",
-          "KU271",
-          "KU272",
-          "KU273",
-          "KU275",
-          "KU276",
-          "KU499",
-          "KU280",
-          "KU284",
-          "KU285",
-          "KU286",
-          "KU287",
-          "KU288",
-          "KU290",
-          "KU291",
-          "KU295",
-          "KU297",
-          "KU300",
-          "KU301",
-          "KU304",
-          "KU305",
-          "KU312",
-          "KU316",
-          "KU317",
-          "KU318",
-          "KU398",
-          "KU399",
-          "KU400",
-          "KU407",
-          "KU402",
-          "KU403",
-          "KU405",
-          "KU408",
-          "KU440",
-          "KU410",
-          "KU416",
-          "KU417",
-          "KU418",
-          "KU420",
-          "KU421",
-          "KU422",
-          "KU423",
-          "KU425",
-          "KU426",
-          "KU444",
-          "KU430",
-          "KU433",
-          "KU434",
-          "KU435",
-          "KU436",
-          "KU438",
-          "KU441",
-          "KU475",
-          "KU478",
-          "KU480",
-          "KU481",
-          "KU483",
-          "KU484",
-          "KU489",
-          "KU491",
-          "KU494",
-          "KU495",
-          "KU498",
-          "KU500",
-          "KU503",
-          "KU504",
-          "KU505",
-          "KU508",
-          "KU507",
-          "KU529",
-          "KU531",
-          "KU535",
-          "KU536",
-          "KU538",
-          "KU541",
-          "KU543",
-          "KU893",
-          "KU545",
-          "KU560",
-          "KU561",
-          "KU562",
-          "KU563",
-          "KU564",
-          "KU309",
-          "KU576",
-          "KU577",
-          "KU578",
-          "KU445",
-          "KU580",
-          "KU581",
-          "KU599",
-          "KU583",
-          "KU854",
-          "KU584",
-          "KU588",
-          "KU592",
-          "KU593",
-          "KU595",
-          "KU601",
-          "KU604",
-          "KU607",
-          "KU608",
-          "KU609",
-          "KU611",
-          "KU638",
-          "KU614",
-          "KU615",
-          "KU616",
-          "KU619",
-          "KU620",
-          "KU623",
-          "KU624",
-          "KU625",
-          "KU626",
-          "KU630",
-          "KU631",
-          "KU635",
-          "KU636",
-          "KU678",
-          "KU710",
-          "KU680",
-          "KU681",
-          "KU683",
-          "KU684",
-          "KU686",
-          "KU687",
-          "KU689",
-          "KU691",
-          "KU694",
-          "KU697",
-          "KU698",
-          "KU700",
-          "KU702",
-          "KU704",
-          "KU707",
-          "KU729",
-          "KU732",
-          "KU734",
-          "KU736",
-          "KU790",
-          "KU738",
-          "KU739",
-          "KU740",
-          "KU742",
-          "KU743",
-          "KU746",
-          "KU747",
-          "KU748",
-          "KU791",
-          "KU749",
-          "KU751",
-          "KU753",
-          "KU755",
-          "KU758",
-          "KU759",
-          "KU761",
-          "KU762",
-          "KU765",
-          "KU766",
-          "KU768",
-          "KU771",
-          "KU777",
-          "KU778",
-          "KU781",
-          "KU783",
-          "KU831",
-          "KU832",
-          "KU833",
-          "KU834",
-          "KU837",
-          "KU844",
-          "KU845",
-          "KU846",
-          "KU848",
-          "KU849",
-          "KU850",
-          "KU851",
-          "KU853",
-          "KU857",
-          "KU858",
-          "KU859",
-          "KU886",
-          "KU887",
-          "KU889",
-          "KU890",
-          "KU892",
-          "KU895",
-          "KU785",
-          "KU905",
-          "KU908",
-          "KU092",
-          "KU915",
-          "KU918",
-          "KU921",
-          "KU922",
-          "KU924",
-          "KU925",
-          "KU927",
-          "KU931",
-          "KU934",
-          "KU935",
-          "KU936",
-          "KU941",
-          "KU946",
-          "KU976",
-          "KU977",
-          "KU980",
-          "KU981",
-          "KU989",
-          "KU992"
-        ]
-      }
-    },
-    {
-      "code": "Ikä",
-      "selection": {
-        "filter": "item",
-        "values": [
-          "SSS"
-        ]
-      }
-    },
-    {
-      "code": "Sukupuoli",
-      "selection": {
-        "filter": "item",
-        "values": [
-          "SSS"
-        ]
-      }
-    },
-    {
-      "code": "Koulutusaste",
-      "selection": {
-        "filter": "item",
-        "values": [
-          "SSS",
-          "3T8",
-          "3",
-          "4",
-          "5",
-          "6",
-          "7",
-          "8",
-          "9"
-        ]
-      }
-    }
-  ],
-  "response": {
-    "format": "json-stat2"
-  }
-}
-const jsonQuery = {
-    "query": [
-      {
-        "code": "Alue",
-        "selection": {
-          "filter": "agg:_Municipalities in alphabetical order 2023.agg",
-          "values": [
-            "SSS",
-            "KU020",
-            "KU005",
-            "KU009",
-            "KU010",
-            "KU016",
-            "KU018",
-            "KU019",
-            "KU035",
-            "KU043",
-            "KU046",
-            "KU047",
-            "KU049",
-            "KU050",
-            "KU051",
-            "KU052",
-            "KU060",
-            "KU061",
-            "KU062",
-            "KU065",
-            "KU069",
-            "KU071",
-            "KU072",
-            "KU074",
-            "KU075",
-            "KU076",
-            "KU077",
-            "KU078",
-            "KU079",
-            "KU081",
-            "KU082",
-            "KU086",
-            "KU111",
-            "KU090",
-            "KU091",
-            "KU097",
-            "KU098",
-            "KU102",
-            "KU103",
-            "KU105",
-            "KU106",
-            "KU108",
-            "KU109",
-            "KU139",
-            "KU140",
-            "KU142",
-            "KU143",
-            "KU145",
-            "KU146",
-            "KU153",
-            "KU148",
-            "KU149",
-            "KU151",
-            "KU152",
-            "KU598",
-            "KU165",
-            "KU167",
-            "KU169",
-            "KU170",
-            "KU171",
-            "KU172",
-            "KU176",
-            "KU177",
-            "KU178",
-            "KU179",
-            "KU181",
-            "KU182",
-            "KU186",
-            "KU202",
-            "KU204",
-            "KU205",
-            "KU208",
-            "KU211",
-            "KU213",
-            "KU214",
-            "KU216",
-            "KU217",
-            "KU218",
-            "KU224",
-            "KU226",
-            "KU230",
-            "KU231",
-            "KU232",
-            "KU233",
-            "KU235",
-            "KU236",
-            "KU239",
-            "KU240",
-            "KU320",
-            "KU241",
-            "KU244",
-            "KU245",
-            "KU249",
-            "KU250",
-            "KU322",
-            "KU256",
-            "KU257",
-            "KU260",
-            "KU261",
-            "KU263",
-            "KU265",
-            "KU271",
-            "KU272",
-            "KU273",
-            "KU275",
-            "KU276",
-            "KU499",
-            "KU280",
-            "KU284",
-            "KU285",
-            "KU286",
-            "KU287",
-            "KU288",
-            "KU290",
-            "KU291",
-            "KU295",
-            "KU297",
-            "KU300",
-            "KU301",
-            "KU304",
-            "KU305",
-            "KU312",
-            "KU316",
-            "KU317",
-            "KU318",
-            "KU398",
-            "KU399",
-            "KU400",
-            "KU407",
-            "KU402",
-            "KU403",
-            "KU405",
-            "KU408",
-            "KU440",
-            "KU410",
-            "KU416",
-            "KU417",
-            "KU418",
-            "KU420",
-            "KU421",
-            "KU422",
-            "KU423",
-            "KU425",
-            "KU426",
-            "KU444",
-            "KU430",
-            "KU433",
-            "KU434",
-            "KU435",
-            "KU436",
-            "KU438",
-            "KU441",
-            "KU475",
-            "KU478",
-            "KU480",
-            "KU481",
-            "KU483",
-            "KU484",
-            "KU489",
-            "KU491",
-            "KU494",
-            "KU495",
-            "KU498",
-            "KU500",
-            "KU503",
-            "KU504",
-            "KU505",
-            "KU508",
-            "KU507",
-            "KU529",
-            "KU531",
-            "KU535",
-            "KU536",
-            "KU538",
-            "KU541",
-            "KU543",
-            "KU893",
-            "KU545",
-            "KU560",
-            "KU561",
-            "KU562",
-            "KU563",
-            "KU564",
-            "KU309",
-            "KU576",
-            "KU577",
-            "KU578",
-            "KU445",
-            "KU580",
-            "KU581",
-            "KU599",
-            "KU583",
-            "KU854",
-            "KU584",
-            "KU588",
-            "KU592",
-            "KU593",
-            "KU595",
-            "KU601",
-            "KU604",
-            "KU607",
-            "KU608",
-            "KU609",
-            "KU611",
-            "KU638",
-            "KU614",
-            "KU615",
-            "KU616",
-            "KU619",
-            "KU620",
-            "KU623",
-            "KU624",
-            "KU625",
-            "KU626",
-            "KU630",
-            "KU631",
-            "KU635",
-            "KU636",
-            "KU678",
-            "KU710",
-            "KU680",
-            "KU681",
-            "KU683",
-            "KU684",
-            "KU686",
-            "KU687",
-            "KU689",
-            "KU691",
-            "KU694",
-            "KU697",
-            "KU698",
-            "KU700",
-            "KU702",
-            "KU704",
-            "KU707",
-            "KU729",
-            "KU732",
-            "KU734",
-            "KU736",
-            "KU790",
-            "KU738",
-            "KU739",
-            "KU740",
-            "KU742",
-            "KU743",
-            "KU746",
-            "KU747",
-            "KU748",
-            "KU791",
-            "KU749",
-            "KU751",
-            "KU753",
-            "KU755",
-            "KU758",
-            "KU759",
-            "KU761",
-            "KU762",
-            "KU765",
-            "KU766",
-            "KU768",
-            "KU771",
-            "KU777",
-            "KU778",
-            "KU781",
-            "KU783",
-            "KU831",
-            "KU832",
-            "KU833",
-            "KU834",
-            "KU837",
-            "KU844",
-            "KU845",
-            "KU846",
-            "KU848",
-            "KU849",
-            "KU850",
-            "KU851",
-            "KU853",
-            "KU857",
-            "KU858",
-            "KU859",
-            "KU886",
-            "KU887",
-            "KU889",
-            "KU890",
-            "KU892",
-            "KU895",
-            "KU785",
-            "KU905",
-            "KU908",
-            "KU092",
-            "KU915",
-            "KU918",
-            "KU921",
-            "KU922",
-            "KU924",
-            "KU925",
-            "KU927",
-            "KU931",
-            "KU934",
-            "KU935",
-            "KU936",
-            "KU941",
-            "KU946",
-            "KU976",
-            "KU977",
-            "KU980",
-            "KU981",
-            "KU989",
-            "KU992"
-          ]
-        }
-      },
-      {
-        "code": "Ikä",
-        "selection": {
-          "filter": "item",
-          "values": [
-            "SSS"
-          ]
-        }
-      },
-      {
-        "code": "Sukupuoli",
-        "selection": {
-          "filter": "item",
-          "values": [
-            "SSS",
-            "1",
-            "2"
-          ]
-        }
-      },
-      {
-        "code": "Vuosi",
-        "selection": {
-          "filter": "item",
-          "values": [
-            "2022"
-          ]
-        }
-      }
-    ],
-    "response": {
-      "format": "json-stat2"
-    }
-  }
-    
 
 
 async function fetchData(){
@@ -739,7 +15,7 @@ async function onLoad(){
     const educationData = await GetChartData();
     const names = population.dimension.Alue.category.label;
     const populationArrays = makeArrays(population.value);
-    // console.log(total)
+    
     let map = L.map('map', {
         minZoom: -3
     });
@@ -784,12 +60,16 @@ async function onLoad(){
     }).addTo(map);
     map.fitBounds(layer.getBounds());
 }
+
+
 // Resets the layer color to blue
 function resetLayer(layer){
     layer.setStyle({
         color: 'blue'
     })
 }
+
+
 // Create chart for the desired location
 async function createCharts(name, populationArrays, index,educationData){
     const mapElement = document.getElementById('map');
@@ -811,12 +91,11 @@ async function createCharts(name, populationArrays, index,educationData){
     Button.addEventListener('click', () => window.location.reload());
     calculateButton.addEventListener('click',() =>updateChart(eduChart,populationArrays.total[index], chart));
     exportButton.addEventListener('click', () => {chart.export(); eduChart.export()});
-    
-    
-    
 
-    
-} //If are is clicked button is made to refresh the page back to big map
+} 
+
+
+//function to make buttons
 function makeButton(){
     const parentdiv= document.getElementById('titleAndButtons');
     
@@ -857,6 +136,8 @@ async function getPopulation(){
     return data = await response.json();
     
 }
+
+
 // finds area code for with the name
 function getKeyByName(population, name) {
     for (const key in population) {
@@ -867,6 +148,9 @@ function getKeyByName(population, name) {
     return 
   }
 //separate population to total, men and female arrays
+
+
+
 function makeArrays(data){
     let total = []
     let men = []
@@ -898,6 +182,9 @@ function makeArrays(data){
     
     return {total, men, female}
 }
+
+
+
 // build the actuial chart with given data
 function buildChart(populationArrays, index, name){
     const chartsTitle =document.getElementById("chartTitle");
@@ -930,6 +217,10 @@ function buildChart(populationArrays, index, name){
       });
       return chart;
 }
+
+
+
+
 async function educationChart(name,index,educationData){
  
   const calculatedIndex = (index*9);
@@ -972,11 +263,18 @@ async function educationChart(name,index,educationData){
   
   
 }
+
+
+
 function exportCharts(chart,genderChart){
   console.log("moi");
   chart.export();
   genderChart.export();
 }
+
+
+
+
 function updateChart(chart,population, genderChart){
   
   if( chart.data.datasets[0].values[0]<100){
@@ -1001,7 +299,6 @@ function updateChart(chart,population, genderChart){
         }
     ],
 
-   
     }
   const genderData ={
     labels: genderLabels,
@@ -1017,6 +314,10 @@ function updateChart(chart,population, genderChart){
   chart.update(educationData);
   
 }
+
+
+
+
 async function GetChartData(){
   const url = 'https://statfin.stat.fi:443/PxWeb/api/v1/en/StatFin/vkour/statfin_vkour_pxt_12bq.px'
   
@@ -1031,4 +332,6 @@ async function GetChartData(){
   return  data  =  await response.json();
 
 }
+
+
 onLoad()
